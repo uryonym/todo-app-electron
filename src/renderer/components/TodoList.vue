dd007fda7809644aca31fd52e9f3b05f895371cc
ryo yoneyama <uryonet@gmail.com>
2019-08-23 23:31:44 +0900

    Todoリストを作成

--- src/renderer/components/TodoList.vue
+++ src/renderer/components/TodoList.vue
@@ -1,13 +1,43 @@
 <template>
-
+  <el-main>
+    <el-input v-model="input" placeholder="タスク名を入力">
+      <el-button slot="append" size="mini" @click="insertTask">追加</el-button>
+    </el-input>
+    <el-table :data="taskList" :show-header="false" stripe>
+      <el-table-column prop="task" width="auto"></el-table-column>
+      <el-table-column align="center" width="100%">
+        <template slot-scope="record">
+          <el-button size="mini" type="danger" @click="deleteTask(record.$index)">削除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-main>
 </template>
 
 <script>
   export default {
-    name: 'TodoList'
+    name: 'TodoList',
+    data () {
+      return {
+        taskList: [],
+        input: ''
+      }
+    },
+    methods: {
+      insertTask () {
+        this.taskList.push({task: this.input})
+        this.input = ''
+      },
+      deleteTask (index) {
+        this.taskList.splice(index, 1)
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  main {
+    width: 600px;
+    margin: 0 auto;
+  }
 </style>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'todo-page',
+      component: require('@/components/TodoList').default
     },
     {
       path: '*',
